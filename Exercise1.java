2ae6d760578fe48c1f51366a7bb4f36bf51ba5c8
ainelally5 <55436454+ainelally5@users.noreply.github.com>
2020-04-02 18:14:28 +0100

    exercise1

---
+++ Exercise1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise1{
+        public static void main(String[] args){
+            //uncomment next line if program requires input
+            	Scanner input = new Scanner(System.in);
+
+                String name = "Mary";
+                String color = "red";
+            	int age = 20;
+            	double weight = 65.48;
+
+            	System.out.println(name+" is " +age+" years old.");
+
+
+            	System.out.println("She weighs "+weight+" kilograms.");
+
+
+            	System.out.println("Her favourite colour is " +color+".");
+
+
+
+	  }
+}
